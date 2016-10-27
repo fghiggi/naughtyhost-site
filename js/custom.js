a5c123ca5d55fb6c2bba684a34bd8c6dfa77afe2
Gustavo Schüler <luis.schuler@gmail.com>
2016-10-27 14:54:18 -0200

    ta pronto

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    
+    //var url = "http://localhost:8080/naughtyhost/clientes";
+    var url = "https://naughtyhost-slave00.c9users.io/clientes";
+
+    function getData() {
+        $.get(url, function(data) {
+            $.each(data, function(key, value) {
+                if (value.pago == 0) {
+                    $(".kkk").append('<option value="'+ value.uuid +'">'+ value.host +'</option>');
+                }
+            });
+        });
+    }
+
+    getData();
+
+    $("#frm_contratar").submit(function(e) {
+    
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#frm_contratar").serialize(),
+            success: function(data) {
+                alert(data.message);
+                document.getElementById('frm_contratar').reset();
+            }
+        });
+
+        e.preventDefault();
+    });
+
+    $("#frm_pagar").submit(function(e) {
+    
+        var uuid = $('select[name=client_id]').val();
+        
+        $.ajax({
+            type: "PUT",
+            url: url,
+            data: {"uuid": uuid},
+            success: function(data) {
+                alert(data.message);
+                location.reload();
+            }
+        });
+
+        e.preventDefault();
+    });
+
+});
+
+
+
--- js/init.js
+++ js/init.js
@@ -6,3 +6,13 @@
     $('select').material_select();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 90 && e.ctrlKey) {
+        $('#escondido').css('display', 'inline-block');
+    }
+
+    if (e.keyCode == 88 && e.ctrlKey) {
+        $('#escondido').css('display', 'none');
+    }
+});
